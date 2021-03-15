9fd73d8f891835318b49d78960af98264ac08622
JuanCalixto <juancalixtomorales@gmail.com>
2021-03-14 21:36:53 -0500

    add onload, focus, mouseenter events and event propagation

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('click', (event) => {
   logoHeading.style.color = 'grey';
+  event.preventDefault()
 });
 
 // Logo Heading Double click Event - Change color to red
@@ -20,19 +21,49 @@ introParagraph.addEventListener('mouseout', (event) => {
   introParagraph.style.transform = 'scale(1)';
 });
 
-// const header = document.querySelector('.main-navigation');
-// header.addEventListener('focus', (event) => {
-//   header.style.color = 'blue';
-//   header.style.fontWeight = 'bold';
-// })
+const header = document.querySelector('.main-navigation');
+header.addEventListener('focus', (event) => {
+  header.style.color = 'blue';
+  header.style.fontWeight = 'bold';
+})
 
 // Wheel event on body - Change color on scroll
 const body = document.querySelector('body');
 body.addEventListener('wheel', () => {
   body.style.backgroundColor = "pink";
-})
+});
 
 // Key event on keydown back to default
 body.addEventListener('keydown', () => {
   body.style.backgroundColor = 'white';
+});
+
+// On load event color change
+window.onload = (event) => {
+  body.style.backgroundColor = 'salmon';
+};
+
+// Nav links mouseenter event
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(l => {
+  l.addEventListener('mouseover', () => {
+    l.style.color = 'red';
+    setTimeout(() => {
+      l.style.color = 'gray';
+    }, 500);
+      //Prevent Default
+  l.addEventListener("click", event => {
+    event.preventDefault();
+  });
 })
+  });
+
+ // Event Propagation
+const textDiv = document.querySelector('.text-content')
+textDiv.addEventListener('click', event => {
+  event.target.style.backgroundColor = 'orange'
+}, false)
+const textP = document.querySelector('.text-content p')
+textP.addEventListener('click', event => {
+  event.target.style.backgroundColor = "green"
+}) 
